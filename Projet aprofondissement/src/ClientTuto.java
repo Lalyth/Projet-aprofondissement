6ef44a19f72233b6f678750d24f64b1169a90367
Lalyth <Lalyth@DESKTOP-M0R4R0U>
2020-05-12 19:33:41 -0400

    Server / Client Tuto

--- Projet aprofondissement/src/ClientTuto.java
+++ Projet aprofondissement/src/ClientTuto.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
+public class ClientTuto {
 
 	public static void main(String[] args) throws IOException {
 		
--- Projet aprofondissement/src/ServerTuto.java
+++ Projet aprofondissement/src/ServerTuto.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.util.Date;
 
-public class Server {
+public class ServerTuto {
 
 	public static void main(String[] args) throws IOException {
 	
