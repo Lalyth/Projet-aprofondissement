703e9e865aaeb260d9745422bd352c204b6b5428
Lalyth <Lalyth@DESKTOP-M0R4R0U>
2020-06-26 14:15:38 -0400

    Ça fonctionne !

---
+++ Projet aprofondissement/src/phil/Client.java
@@ -0,0 +1,121 @@
+package phil;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+/* Client pour Projet Approfondissement
+ * Auteur : Philippe Lamarche & Alexandre Blouin
+ * Date : 14 Mai 2020 */
+
+public class Client {
+	static Scanner scan;
+	static DataInputStream in;
+	static DataOutputStream out;
+	static String monId;
+	//static String autreId;
+	static int pv;
+
+	static String messageRecu = "";
+
+	static String nom = "";
+	private static boolean vivant;
+
+	static final String MENU = "Appuyer sur [1] pour attaquer, [2] pour notCodedYet, [3] pour notCodedYet";
+	static final String ATT = "Appuyer sur [1] pour faire 1 de dégats, [2] pour faire 3 de dégats, [3] pour faire 5 de dégats";
+
+
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("IP comme argument");
+			return;
+		}
+
+		try (var socket = new Socket(args[0], 59090)) { // Initiate connection
+			scan = new Scanner(System.in);
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+			monId = in.readUTF();
+			pv = in.readInt();
+
+			System.out.println("- - Connexion établie avec le serveur - - Vous êtes joueur #" + monId);
+			System.out.println("PV : " + pv);
+			System.out.println("Bonjour, quel est votre nom ?");
+
+			nom = scan.nextLine();			
+			out.writeUTF(nom);
+
+			vivant = true;
+
+			while (vivant) {				
+				messageRecu = in.readUTF(); 
+				if (messageRecu.startsWith(monId + " MENU")) {
+					menu();
+				}
+				if (messageRecu.startsWith(monId + " PV")) {
+					pv = Integer.parseInt(messageRecu.substring(5));
+				}
+				if (pv < 1) {
+					out.writeUTF(monId + " DEAD");
+					vivant = false;
+				}
+			}
+
+		}
+	}
+
+
+	private static void menu() {
+		System.out.println(MENU);
+		System.out.println("PV : " + pv);
+
+		String choix = scan.nextLine();
+
+		switch(choix) {
+			case "1" :
+				attaque();
+				break;
+			case "2" :
+				equipe();
+				break;
+			case "3" :
+				item();
+			default:
+				System.out.println("Choix valide svp");
+		}
+
+	}
+
+	private static void attaque() {
+		System.out.println(ATT);
+		try {
+			String choix = scan.nextLine();
+
+			switch (choix) {
+				case "1" :
+					out.writeUTF("ATT " + monId + " 0");
+					break;
+				case "2" :
+					out.writeUTF("ATT " + monId + " 1");
+					break;
+				case "3" :
+					out.writeUTF("ATT " + monId + " 2");
+					break;
+				default : 
+					System.out.println("");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void equipe() {
+
+	}
+
+	private static void item() {
+
+	}
+}
---
+++ Projet aprofondissement/src/phil/Server.java
@@ -0,0 +1,138 @@
+package phil;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.Executors;
+
+/* Serveur pour Projet Approfondissement
+ * Auteur : Philippe Lamarche & Alexandre Blouin
+ * Date : 14 Mai 2020 */
+
+// ** Tout commentaire entre parenthèse provient des tooltips d'Eclipse ** \\
+
+public class Server {
+	static String messageRecu;
+	static int nbrJoueurs;
+	static final int[] ATTAQUE = {1,3,5};
+
+	private static ArrayList<DataOutputStream> writers = new ArrayList<DataOutputStream>();
+	private static ArrayList<Joueur> joueurs = new ArrayList<Server.Joueur>();
+
+	public static void main(String[] args) throws Exception {
+		try (var listener = new ServerSocket(59090)) {
+			System.out.println("Server status : Barely Working");
+			nbrJoueurs = 0;
+			var pool = Executors.newFixedThreadPool(2); // Nombre de socket disponible dans le pool
+			while (true) {
+				nbrJoueurs++;
+				pool.execute(new Joueur(listener.accept(), nbrJoueurs, 10)); // "Listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made."	
+			}
+
+		}
+	}
+
+	private static class Joueur implements Runnable {
+		private Socket socket; // "A socket is an endpoint for communication between two machines."
+		private int id;
+		private String nom;
+		private int pv;
+		private boolean vivant;
+
+		public int getPv() {
+			return pv;
+		}
+
+		public void setPv(int pv) {
+			this.pv = pv;
+		}
+
+		public boolean isVivant() {
+			return vivant;
+		}
+
+		public void setVivant(boolean vivant) {
+			this.vivant = vivant;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public String getNom() {
+			return nom;
+		}
+
+		public String toString() {
+			return String.format("Nom : %S PV : %d, ID : %d", nom, pv, id);
+		}
+
+		Joueur(Socket socket, int id, int pv) { // Constructor qui va etre appelé pour créer un joueur
+			this.socket = socket;
+			this.id = id;
+			this.pv = pv;
+		}
+
+		@Override
+		public void run() {
+			joueurs.add(this);
+			System.out.println("Joueur " + id + " se connecte - Connection avec: " + socket);
+
+			try {
+				var in = new DataInputStream(socket.getInputStream());
+				var out = new DataOutputStream(socket.getOutputStream());
+				writers.add(out); // Ajout du 'out' dans une liste pour pouvoir envoyer des messages à tous les joueurs
+				out.writeUTF(Integer.toString(id));
+				out.flush();
+				out.writeInt(pv);
+				out.flush();
+
+				messageRecu = in.readUTF();
+				nom = messageRecu;
+				System.out.println(nom + " s'ajoute à la partie");				
+				vivant = true;
+
+				out.writeUTF("1 MENU");
+
+				while (vivant) {
+					messageRecu = in.readUTF();
+					if (messageRecu.startsWith("ATT")) {
+						if (messageRecu.substring(4).startsWith("1")) {
+							int atkId = Integer.parseInt(messageRecu.substring(6));
+							joueurs.get(1).setPv(joueurs.get(1).getPv() - ATTAQUE[atkId]);
+							for (DataOutputStream o : writers) {
+								o.writeUTF("2 PV " + joueurs.get(1).getPv());
+								o.writeUTF("2 MENU");
+							}
+						} else if (messageRecu.substring(4).startsWith("2")) {
+							int atkId = Integer.parseInt(messageRecu.substring(6));
+							joueurs.get(0).setPv(joueurs.get(0).getPv() - ATTAQUE[atkId]);
+							for (DataOutputStream o : writers) {
+								o.writeUTF("1 PV " + joueurs.get(0).getPv());
+								o.writeUTF("1 MENU");
+							}
+						}
+					}
+				}
+
+			} catch (IOException e) {
+				System.out.println("Erreur socket:" + socket);
+				e.printStackTrace();
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+				System.out.println("Fermeture du socket: " + socket);
+				nbrJoueurs--;
+			}
+		}
+	}
+}
